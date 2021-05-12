e3395760aed782f3f717b1bb5f546d9778a9daae
tuckerTU <tuckerishere@gmail.com>
2021-05-12 11:02:30 -0600

    create log out nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
 
+    get '/login' do
+        erb :"sessions/new"
+    end
+
+    post '/sessions' do
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/'
+        else
+            redirect '/login'
+        end
+    end
+
+
     get '/logout' do
         session.clear 
         redirect '/'
