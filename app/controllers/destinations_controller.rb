50b3ed1f8b465e1957a547d298483427f54aea7e
tuckerTU <tuckerishere@gmail.com>
2021-05-18 12:46:15 -0600

    work on edit page

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,17 +1,33 @@
 class DestinationsController < ApplicationController
 
+    get '/destinations/new' do
+        erb :'destinations/new'
+    end
+
+    post '/destinations' do
+      destination = current_user.destinations.create(params{:destination})
+      redirect "destinations/#{destination.id}"
+    end
+
     get '/destinations' do 
         @destinations = Destination.all
         erb :'destinations/index'
     end
 
-    get '/destinations/new' do
-        erb :'destinations/new'
-    end
-
     get '/destinations/:id' do 
         @destination = Destination.find(params[:id])
         erb :'destinations/show'
+    end
+
+    get '/destinations/:id/edit' do 
+        @destination = Destination.find(params{:id})
+        erb :'destinations/edit'
+    end
+
+    patch '/destinations/:id' do 
+        destination = Destination.find(params{:id})
+        destination.update(params{:destination})
+        redirect '/destinations/#{destination.id}'
     end
 
 end
