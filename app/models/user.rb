555f333e01a37de74141c3e4fbe4273d9c0788e0
tuckerTU <tuckerishere@gmail.com>
2021-05-19 10:31:34 -0600

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :destinations, through: :users_destinations
     validates :username, uniqueness: :true
+    validates :password, length: { in: 6..20 }
 end
