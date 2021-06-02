8656e27596355c5a348c915b1e587aa78bb12b2a
tuckerTU <tuckerishere@gmail.com>
2021-06-02 13:22:01 -0600

    allow only users who added a destination the ability to edit/delete destination

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -21,25 +21,33 @@ class DestinationsController < ApplicationController
     end
 
     get '/destinations/:id' do 
+        if logged_in?
         @destination = Destination.find(params[:id])
         erb :'destinations/show'
+        end
     end
 
     get '/destinations/:id/edit' do 
+        if logged_in?
         @destination = Destination.find(params{:id})
         erb :'destinations/edit'
+        end
     end
 
     patch '/destinations/:id' do 
+        if logged_in?
         destination = Destination.find(params{:id})
         destination.update(params{:destination})
         redirect '/destinations/#{destination.id}'
+        end
     end
 
-    delete '/destinations/:id' do 
+    delete '/destinations/:id' do
+        if logged_in? 
         destination = Destination.find(params[:id])
         destination.destroy
         redirect '/destinations'
+        end
     end
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,4 @@
 class Destination < ActiveRecord::Base
-    has_many :users, through: :user_destinations
     has_many :user_destinations
+    has_many :users, through: :user_destinations
 end
